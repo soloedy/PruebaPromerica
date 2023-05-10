7b2d11db313a47a57a746e3a18b79cad87ad2e09
Edy Tobar <32673751+soloedy@users.noreply.github.com>
2023-05-10 15:49:10 -0600

    Add files via upload

---
+++ WinConexionBaseDatos/WinConexionBaseDatos/Form1.Designer.cs
@@ -0,0 +1,89 @@
+﻿
+namespace WinConexionBaseDatos
+{
+    partial class Prueba
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.btnConexion = new System.Windows.Forms.Button();
+            this.btnActualizar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(444, 102);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(8, 8);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // btnConexion
+            // 
+            this.btnConexion.Location = new System.Drawing.Point(12, 30);
+            this.btnConexion.Name = "btnConexion";
+            this.btnConexion.Size = new System.Drawing.Size(154, 80);
+            this.btnConexion.TabIndex = 1;
+            this.btnConexion.Text = "Conectar BD - Select Dataset";
+            this.btnConexion.UseVisualStyleBackColor = true;
+            this.btnConexion.Click += new System.EventHandler(this.btnConexion_Click);
+            // 
+            // btnActualizar
+            // 
+            this.btnActualizar.Location = new System.Drawing.Point(189, 30);
+            this.btnActualizar.Name = "btnActualizar";
+            this.btnActualizar.Size = new System.Drawing.Size(154, 80);
+            this.btnActualizar.TabIndex = 2;
+            this.btnActualizar.Text = "Conectar BD - UPDATE Filas Afectadas";
+            this.btnActualizar.UseVisualStyleBackColor = true;
+            this.btnActualizar.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Prueba
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(365, 134);
+            this.Controls.Add(this.btnActualizar);
+            this.Controls.Add(this.btnConexion);
+            this.Controls.Add(this.button1);
+            this.Name = "Prueba";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Button btnConexion;
+        private System.Windows.Forms.Button btnActualizar;
+    }
+}
+
---
+++ WinConexionBaseDatos/WinConexionBaseDatos/Form1.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinConexionBaseDatos
+{
+    public partial class Prueba : Form
+    {
+        
+        public Prueba()
+        {
+            InitializeComponent();
+        }
+
+        private void btnConexion_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // SE REALIZA CONEXIÓN CON BASE DE DATOS.
+                SqlConnection conexion = new SqlConnection(Properties.Settings.Default.ConnectionString.ToString());
+                SqlCommand cmd = new SqlCommand();
+                DataSet dtsDatos = new DataSet();
+                SqlDataAdapter sqlDa;
+                conexion.Open();
+                // REALIZA CONSULTA HACIA BASE DE DATOS.
+                cmd.CommandText = "SELECT * FROM datos.dbo.datos_tarjeta";
+                cmd.CommandType = CommandType.Text;
+                cmd.Connection = conexion;
+
+                sqlDa = new SqlDataAdapter(cmd);
+                sqlDa.Fill(dtsDatos);
+
+                // VALIDA SI LA CONSULTA DEVOLVIÓ RESULTADOS.
+                if (dtsDatos.Tables[0].Rows.Count > 0)
+                {
+                    MessageBox.Show("Número de filas - " + dtsDatos.Tables[0].Rows.Count);
+                }
+                else
+                {
+                    MessageBox.Show("La consulta no devolvió resultados.");
+                }
+
+                // CERRAMOS CONEXIÓN CON BASE DE DATOS.
+                conexion.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error en proceso de conexión y consulta hacia base de datos. " + ex.Message);
+            }
+        }
+
+        // ESTE MISMO PROCESO SE PUEDE REALIZAR PARA EL CRUD COMPLETO.
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // SE REALIZA CONEXIÓN CON BASE DE DATOS.
+                SqlConnection conexion = new SqlConnection(Properties.Settings.Default.ConnectionString.ToString());
+                SqlCommand cmd = new SqlCommand();
+                DataSet dtsDatos = new DataSet();
+                SqlDataAdapter sqlDa;
+                conexion.Open();
+                // REALIZA LA TAREA HACIA BASE DE DATOS.
+                cmd.CommandText = "DELETE FROM datos.dbo.datos_tarjeta";
+                cmd.CommandType = CommandType.Text;
+                cmd.Connection = conexion;
+
+                sqlDa = new SqlDataAdapter(cmd);
+
+                // DEVUELVE EL NÚMERO DE FILAS AFECTADAS POR EL DELETE.
+                cmd.CommandText = "SELECT @@ROWCOUNT AS 'FilasAfectadas'";
+                cmd.CommandType = CommandType.Text;
+                cmd.Connection = conexion;
+
+                sqlDa = new SqlDataAdapter(cmd);
+                sqlDa.Fill(dtsDatos);
+
+                // VALIDA CUANTAS FILAS FUERON AFECTADAS.
+                if (dtsDatos.Tables[0].Rows.Count > 0)
+                {
+                    MessageBox.Show("Número de filas afectadas - " + dtsDatos.Tables[0].Rows[0]);
+                }
+                else
+                {
+                    MessageBox.Show("La consulta no afectó ninguna de las filas.");
+                }
+
+                // CERRAMOS CONEXIÓN CON BASE DE DATOS.
+                conexion.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error en proceso de conexión y consulta hacia base de datos. " + ex.Message);
+            }
+        }
+    }
+}
